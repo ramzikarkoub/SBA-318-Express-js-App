3db71aa193e589469c610b15569ed34be04b46e6
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-01-21 22:37:48 -0500

    added Post request to posts route

--- routes/posts.js
+++ routes/posts.js
@@ -9,16 +9,22 @@ router.get("/", () => {
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  console.log(id);
   const parsedId = parseInt(id);
-  // console.log(req.params);
-  // console.log("posts", posts);
   const post = posts.find((post) => post.id === parsedId);
   console.log(post);
   if (!post) {
     return res.status(404).send({ message: "post does Not exist" });
   }
   return res.status(200).send(post);
+});
+router.post("/", (req, res) => {
+  const post = req.body;
+  const newPosts = posts.push(post);
+  if (!req.user) return res.sendStatus(401);
+  req.logout((err) => {
+    if (err) return res.sendStatus(400);
+    res.status(200).send(newPosts);
+  });
 });
 
 module.exports = router;
