f62c5f0632043e06f83d9ea1f0e08c2aebf6c608
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-01-21 21:30:39 -0500

    added posts/users routes

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const PORT = 8000;
 app.use(express.json());
 
 const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
-// app.use("/users", () => {
-//   console.log("usersss");
-// });
 app.use("/products", productsRouter);
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 // app.use("/posts", () => {
 //   console.log("posts");
 // });
